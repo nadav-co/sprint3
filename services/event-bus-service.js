6d68c2622b00b7984ecb53cda626df1ac299d87e
Nadav Cohen <nadav1999@gmail.com>
2020-12-23 12:56:56 +0200

    added utils bus-service and storage service

---
+++ services/event-bus-service.js
@@ -0,0 +1,23 @@
+export const bus = {
+    on,
+    emit
+}
+
+function on(eventName, listener) {
+
+    const callListener = ({ detail }) => {
+        listener(detail);
+    };
+    window.addEventListener(eventName, callListener);
+
+    return () => {
+        window.removeEventListener(eventName, callListener);
+    };
+}
+
+function emit(eventName, data) {
+    window.dispatchEvent(new CustomEvent(eventName, { detail: data }));
+}
+
+
+export const eventBusService = { on, emit };
